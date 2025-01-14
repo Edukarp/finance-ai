ab417d78ce3aeb9841a8697f762480ccf9468848
Eduardo Karpfenstein <edukarp10@gmail.com>
2025-01-14 17:09:22 -0300

    feat: add transaction initial table

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,9 +1,24 @@
-const TransactionsPage = () => {
-    return (
-        <div>
-            <h1>Transactions</h1>
-        </div>
-    );
-}
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { transactionColumns } from "./_columns";
+import { DataTable } from "../_components/ui/data-table";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1>Transações</h1>
+
+        <Button className="rounded-full">
+          Adicionar Transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
+};
 
 export default TransactionsPage;
